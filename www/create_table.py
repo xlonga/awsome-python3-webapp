64d29f2ef50b2e25422dfc1120c1e42d1448e30a
long <410813026@qq.com>
2019-10-18 17:58:14 +0800

    第一次提交Day1-4

---
+++ www/create_table.py
@@ -0,0 +1,19 @@
+
+
+import orm
+import asyncio
+from models import User, Blog, Comment
+
+async def test(loop):
+	await orm.create_pool(loop=loop, host='127.0.0.1', user='root', password='123456', db='awesome')
+
+	u = User(name='Test', email='test@example.com', passwd='1234567890', image='about:blank')
+
+	await u.save()
+	# await orm.destory_pool()
+if __name__ == '__main__':
+
+	loop = asyncio.get_event_loop()
+	loop.run_until_complete(test(loop))
+	print('Test finished')
+	# loop.close()
