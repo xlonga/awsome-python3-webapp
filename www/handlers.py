4400f6c122afe342af99536ec943465533021e5d
long <410813026@qq.com>
2019-10-22 14:22:57 +0800

    Day8 构建前端，显示blogs

--- www/handlers.py
+++ www/handlers.py
@@ -11,10 +11,23 @@ from coroweb import get, post
 
 from models import User, Comment, Blog, next_id
 
+# @get('/')
+# async def index(request):
+# 	users = await User.findAll()
+# 	return {
+# 		'__template__': 'test.html',
+# 		'users': users
+# 	}
+
 @get('/')
-async def index(request):
-	users = await User.findAll()
+def index(request):
+	summary = 'Lorem ipsum dolor sit amet, consetetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+	blogs = [
+		Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+		Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+		Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+	]
 	return {
-		'__template__': 'test.html',
-		'users': users
+		'__template__': 'blogs.html',
+		'blogs': blogs
 	}
